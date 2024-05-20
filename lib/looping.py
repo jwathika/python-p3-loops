026587eeef658eb5617f4e6ce68436ebea9e9b1b
wathika-eng <wathika02@gmail.com>
2024-05-20 15:34:04 +0300

    feat: Add square_integers function to looping.py
    This commit adds the `square_integers` function to the `looping.py` file. The function takes a list of integers as input and returns a new list with each integer squared. It also includes print statements to display the squared values.

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
+
 def happy_new_year():
-    # code goes here!
-    pass
+    year = 0
+    while year < 10:
+        year += 1
+        print(year)
+        print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for i in int_list:
+        new_i = i * i
+        squared_list.append(new_i)
+        print(new_i)
+    return squared_list
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
